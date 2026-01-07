a92427919cbd986c6dcfb865fb034338684997c8
Leonardo Munarolo <leonardodm510@gmail.com>
2026-01-07 12:56:05 -0300

    Initial Commit

---
+++ Poof!/Poof_App.swift
@@ -0,0 +1,17 @@
+//
+//  Poof_App.swift
+//  Poof!
+//
+//  Created by Leonardo Munarolo on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Poof_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
